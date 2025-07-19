df0839d1be1713e53142a0564898928d70a8cfa7
josemg08 <josemg08@gmail.com>
2025-07-19 10:45:25 -0300

    Clase 4 edit text

--- app/src/main/java/com/example/presentationcard/EducationActivity.java
+++ app/src/main/java/com/example/presentationcard/EducationActivity.java
@@ -3,6 +3,11 @@ package com.example.presentationcard;
 import static com.example.presentationcard.Constants.EXTRA_STRING_KEY;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EducationActivity extends AppCompatActivity {
@@ -13,6 +18,28 @@ public class EducationActivity extends AppCompatActivity {
 
         // Get the string extra from the intent
         String receivedString = getIntent().getStringExtra(EXTRA_STRING_KEY);
+
+        // Getting the edit text input
+        EditText editText = findViewById(R.id.edit_text);
+        TextView educationInfo = findViewById(R.id.education_text);
+
+        // Add TextWatcher to monitor EditText changes
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Update TextView when text changes
+                educationInfo.setText(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // do nothing
+            }
+        });
     }
 }
-
